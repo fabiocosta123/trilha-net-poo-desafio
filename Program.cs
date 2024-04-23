d82559c5989a81a7bf3d4b5381923ed47e733f0c
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2024-04-23 12:03:59 -0300

    implementando código Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Nokia n1 = new Nokia(numero:"13996285971", modelo:"Nokia e30+", imei:"252512525", memoria: 8);
+n1.InstalarAplicativo("Facebook");
+
+Console.WriteLine("==========================================");
+
+Iphone iphone= new Iphone(numero:"13996378295", modelo:"Iphone15", imei: "987654321", memoria: 12);
+iphone.InstalarAplicativo("Linkedin");
